0ae0a4f48c748f2ad2bc3dd6c9156747bd50ae0e
AlfredoHernandez45 <l18390045@chetumal.tecnm.mx>
2023-01-22 14:22:28 -0500

    Api funcionando GET, POST, DELETE

---
+++ app/Http/Controllers/Api/ApiProductoController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductoRequest;
+use App\Models\Producto;
+use Illuminate\Http\Request;
+
+class ApiProductoController extends Controller
+{
+        /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Producto::all();
+
+        return response()->json([
+            'status'=>true,
+            'posts'=> $posts
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    // public function create()
+    // {
+    //     //
+    // }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // Validación
+        $request->validate([
+			'nombre' => 'required',
+			'descripcion' => 'required',
+			'imagen' => 'required|image|mimes:jpeg,png,jpg'
+		]);
+
+        // Obtiene toda la valicación
+        $producto = $request->all();
+
+        // Si se mando una imagen la guarda
+        if ( $imagen = $request->file('imagen') ) {
+			$rutaGuardarImg = 'imagen/';
+			$imagenProducto = date('YmdHis').".".$imagen->getClientOriginalExtension();
+			$imagen->move($rutaGuardarImg, $imagenProducto);
+			$producto['imagen'] = "$imagenProducto";
+		}
+
+        // Crea el nuevo producto
+        Producto::create($producto);
+
+        // Devuelte en respuesta el producto creado
+        return response()->json([
+            'status'=>true,
+            'modelos'=> $producto
+        ]); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    // public function show(Post $post)
+    // {
+    //     //
+    // }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    // public function edit(Post $post)
+    // {
+    //     //
+    // }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreProductoRequest $request, Producto $post)
+    {
+        $post->update($request->all());
+
+        return response()->json([
+            "status" => true,
+            "message" => "Put Update done successfully",
+            "post" => $post
+        ]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Post  $post
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Producto $producto)
+    {
+        $producto->delete();
+
+        return response()->json([
+            "status" => true,
+            "message" => "Delete Destroy done successfully",
+        ]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\ApiProductoController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//ruta para dar acceso a todas las rutas
+Route::apiResource('productos', ApiProductoController::class);
